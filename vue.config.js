7291ab9cb855b1f495aba9981a156672893272fd
Vitor <vitormoraes200415@gmail.com>
2026-07-10 22:08:38 -0300

    Refactor vue.config.js to use publicPath for production environment

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,7 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  plugins: [vue()],
-  base: '/TagNFCFilamentos/',
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/TagNFCFilamentos/' // Nome exato do seu repositório no GitHub
+    : '/'
 })
