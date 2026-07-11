f00725e44f5365d42f9aa953d884834a1e46df17
Vitor <vitormoraes200415@gmail.com>
2026-07-11 18:36:32 -0300

    Refactor App.vue to dynamically render attributes from URL parameters

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,11 @@
 <template>
   <div style="display: flex; flex-direction: column; align-items: center; margin-top: 20px;">
-    <span>PLA Premium 1,75mm - F3D</span>
-    <span>Filamento de alta qualidade para impressão 3D</span>
-    <span>Fabricante: F3D</span>
-    <span>Diâmetro: 1,75mm</span>
-    <span>Peso: 1kg</span>    
-    <span>Cor: {{ cor }}</span>
-    <span>Temperatura Do Bico: 205-230°C</span>
-    <span>Temperatura Da Mesa: 50-65°C</span>
-    <span>Velocidade impressão: 20-70mm/s</span>
-
-    <span>Configuração Aprovada</span>
-    <span>Temperatura Do Bico: 210°C</span>
-    <span>Temperatura Da Mesa: 60°C</span>
+    <span v-for="(atributo, index) in atributos" :key="index">
+      <div v-for="(valor, chave) in atributo" :key="chave">
+        <strong v-if="chave==='titulo'">{{ upercaseAllWords(replaceUnderscoresWithSpaces(valor)) }}</strong>
+        <strong v-if="chave!=='pular' && chave!=='titulo'">{{ upercaseAllWords(replaceUnderscoresWithSpaces(chave)) }}:</strong> <span v-if="chave!=='pular' && chave!=='titulo'">{{ upercaseAllWords(replaceUnderscoresWithSpaces(valor)) }}</span>
+      </div>
+    </span>
   </div>
 </template>
 
@@ -22,31 +15,22 @@ export default {
   name: 'App',
   data() {
     return {
-      userId: null,
-      cor: ''
+      atributos: []
     }
   },
   components: {
   },
   created() {
     const urlParams = new URLSearchParams(window.location.search);
-    this.userId = urlParams.get('id');//?id=123
-    if (this.userId === '04:4E:AA:7B:C3:2A:81') {
-      this.cor = 'Preto'
-    }else {
-      this.cor = 'Desconhecida'
-    }/* else if (this.userId === '04:4E:AA:7B:C3:2A:81') {
-      this.cor = 'Branco'
-    } else if (this.userId === '04:4E:AA:7B:C3:2A:81') {
-      this.cor = 'Vermelho'
-    } else if (this.userId === '04:4E:AA:7B:C3:2A:81') {
-      this.cor = 'Azul'
-    } else if (this.userId === '04:4E:AA:7B:C3:2A:81') {
-      this.cor = 'Verde'
-    } else {
-      this.cor = 'Desconhecida'
-    }*/
-    //alert(`ID: ${this.userId}`);
+    this.atributos.push(Object.fromEntries(urlParams.entries()))
   },
+  methods: {
+    replaceUnderscoresWithSpaces(str) {
+      return str.replace(/_/g, ' ');
+    },
+    upercaseAllWords(str) {
+      return str.replace(/(^\p{L}|\s\p{L})/gu, char => char.toUpperCase());
+    },
+  }
 }
 </script>
